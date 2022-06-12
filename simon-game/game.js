e656789e571c7020eb08cc67be6c370ffee7f39c
Ahmet Emre Akar <82061314+aea555@users.noreply.github.com>
2022-06-12 03:44:19 +0300

    Add files via upload

---
+++ simon-game/game.js
@@ -0,0 +1,86 @@
+var colors = ["red", "green", "yellow", "blue"]
+var gamePattern = []
+var userClickPattern = []
+var level = 0
+
+let showLevel = () => {
+    $("#level-title").text("Level " + level)
+}
+
+let playAudio = (color) => {
+    let audio = new Audio("sounds/" + color + ".mp3")
+    audio.play()
+}
+
+let animate = (color) => {
+    $("#" + color).fadeIn(300).fadeOut(300).fadeIn(300)
+}
+
+let createRandomColor = () => {
+    let randomColor = colors[Math.floor(Math.random() * 4)]
+    return randomColor
+}
+
+let reset = () => {
+    setTimeout(function() {
+        $("#level-title").text("Game over, click the button below to restart")
+        $("body").addClass("game-over")
+        playAudio("wrong")
+        $(".refresh-button").removeClass("undisplayed")
+        $(".btn").addClass("undisplayed")
+    }, 200)
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+let createStep = () => {
+    let step = createRandomColor()
+    gamePattern.push(step)
+    animate(step)
+    playAudio(step)
+}
+
+let handleClick = (clickedButton) => {
+    let clickedColor = $(clickedButton).attr("id")
+    userClickPattern.push(clickedColor)
+    $("#" + clickedColor).addClass("pressed")
+    playAudio(clickedColor)
+    animate(clickedColor)
+    $("#" + clickedColor).removeClass("pressed")
+}
+
+let check = () => {
+    if (gamePattern.length === userClickPattern.length) {
+        if (checkPattern(gamePattern, userClickPattern)) {
+            level++
+            showLevel()
+            userClickPattern = []
+            setTimeout(function() {
+                createStep()
+            }, 900)
+        } else {
+            reset()
+        }
+    }
+}
+
+let checkPattern = (a, b) => {
+        return Array.isArray(a) &&
+            Array.isArray(b) &&
+            a.length === b.length &&
+            a.every((val, index) => val === b[index]);
+    }
+    ////////////////////////////////////////////////////////////////////////////////////
+let go = () => {
+    createStep()
+    $(".btn").click(function() {
+        handleClick($(this))
+        check()
+    })
+}
+
+$(document).keypress(function() {
+    $("body").removeClass("game-over")
+    level++
+    $("#level-title").text("Level " + level)
+    go()
+})
